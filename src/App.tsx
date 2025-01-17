fb78906b82fc5261b5cfe694144ac4670f400e56
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-01-17 18:19:46 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { Suspense, useContext, useState } from 'react';
+import { Counter } from './components/Counter';
+import './styles/index.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  const bool = true;
+
+  return (
+    <div className={classNames('app', {hovered: true}, [theme])}>
+      <button onClick={toggleTheme}>ToGGLE</button>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
