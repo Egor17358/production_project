376b2abb7869cd8611206b088985dd66490080b8
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-03-15 01:44:02 +0700

    add fetching articles, view type selector

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -27,18 +27,18 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Normal: Story = {
-  args: {
+// export const Normal: Story = {
+//   args: {
 
-  },
-};
+//   },
+// };
 
-export const DARK: Story = {
-  args: {
+// export const DARK: Story = {
+//   args: {
 
-  },
-};
-DARK.decorators = [ThemeDecorator];
-DARK.parameters = {
-  theme: Theme.DARK,
-};
+//   },
+// };
+// DARK.decorators = [ThemeDecorator];
+// DARK.parameters = {
+//   theme: Theme.DARK,
+// };
