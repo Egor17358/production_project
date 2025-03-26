75f751f854997b6b256e06bfd8f6ca268bfcf2de
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-03-26 13:20:11 +0700

    add dropdown, update navbar, add avatar with menu

---
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -0,0 +1,61 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import cls from './Dropdown.module.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Fragment, ReactNode } from 'react';
+import { AnchorProps } from '@headlessui/react/dist/internal/floating';
+import { AppLink } from '../AppLink/AppLink';
+
+export interface DropdownItem {
+  disabled?: boolean;
+  content?: ReactNode;
+  onClick?: () => void;
+  href?: string;
+}
+
+interface DropdownProps {
+  className?: string;
+  items: DropdownItem[];
+  trigger: ReactNode;
+  direction?: AnchorProps;
+}
+
+export function Dropdown(props: DropdownProps) {
+  const { className, items, trigger, direction = 'bottom' } = props;
+
+  return (
+    <Menu as={'div'} className={classNames(cls.Dropdown, {}, [className])}>
+      <MenuButton className={cls.btn}>{trigger}</MenuButton>
+      <MenuItems anchor={direction} className={cls.menu}>
+        {items.map(item => {
+          const content = ({ focus }: { focus: boolean }) => (
+            <button
+              type='button'
+              disabled={item.disabled}
+              onClick={item.onClick}
+              className={classNames(cls.item, { [cls.active]: focus }, [])}
+            >
+              {item.content}
+            </button>
+          );
+
+          if (item.href) {
+            return (
+              <>
+                <MenuItem as={AppLink} to={item.href} disabled={item.disabled}>
+                  {content}
+                </MenuItem>
+              </>
+            );
+          }
+          return (
+            <>
+              <MenuItem as={Fragment} disabled={item.disabled}>
+                {content}
+              </MenuItem>
+            </>
+          );
+        })}
+      </MenuItems>
+    </Menu>
+  );
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -9,6 +9,8 @@ import { getUserAuthData, userActions } from 'entites/User';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Dropdown } from 'shared/ui/Dropdown/Dropdown';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
 
 interface NavbarProps {
   className?: string;
@@ -38,12 +40,28 @@ export const Navbar = memo(({ className }: NavbarProps) => {
     return (
       <header className={classNames(cls.Navbar, {}, [className])}>
         <Text theme={TextTheme.INVERTED} className={cls.appName} title={t('Тест app')} />
-        <AppLink className={cls.createBtn} theme={AppLinkTheme.SECONDARY} to={RoutePath.article_create}>
+        <AppLink
+          className={cls.createBtn}
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.article_create}
+        >
           {t('Создание новой статьи')}
         </AppLink>
-        <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onLogOut}>
-          {t('Выйти')}
-        </Button>
+        <Dropdown
+          direction={'bottom end'}
+          className={cls.dropdown}
+          items={[
+            {
+              content: t('Выйти'),
+              onClick: onLogOut,
+            },
+            {
+              content: t('Профиль'),
+              href: RoutePath.profile + authData.id
+            },
+          ]}
+          trigger={<Avatar size={30} src={authData.avatar} />}
+        />
       </header>
     );
   }
