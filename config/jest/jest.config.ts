180631a284320a5f1012ae081f2ffc2a826578ce
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-03-26 19:16:34 +0700

    add html reporter for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 import path from 'path';
 
 const config: Config = {
@@ -16,44 +16,40 @@ const config: Config = {
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/private/var/folders/9x/_681lhc97c16_383j30d5dn00000gn/T/jest_dx",
 
-
   globals: {
     __IS_DEV__: true,
     __API__: '',
-    __PROJECT__: 'jest'
+    __PROJECT__: 'jest',
   },
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
   // testEnvironment: 'jest-environment-jsdom',
   testEnvironment: 'jsdom',
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
-  moduleDirectories: [
-    "node_modules"
-  ],
- moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "tsx",
-    "json",
-    "node"
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
   },
-  modulePaths: [
-    "<rootDir>src",
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '<rootDir>/reports/unit',
+        filename: 'report.html',
+        openReport: true,
+        inlineSource: true,
+      },
+    ],
   ],
+  modulePaths: ['<rootDir>src'],
   rootDir: '../../',
   setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   testMatch: [
-    "<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)"
+    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     // "**/__tests__/**/*.[jt]s?(x)",
     // "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
@@ -67,7 +63,6 @@ const config: Config = {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
@@ -111,9 +106,7 @@ const config: Config = {
 
   // An array of directory names to be searched recursively up from the requiring module's location
 
-
   // An array of file extensions your modules use
- 
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -179,7 +172,6 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
