015363ea592460e521ff3fccfd70f1103b9d1594
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-05-26 19:02:10 +0700

    add useModal hook, upgrade drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -4,26 +4,39 @@ import { MutableRefObject, ReactNode, useCallback, useEffect, useRef, useState }
 import { Portal } from '../Portal/Portal';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Overlay } from '../Overlay/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 
 export interface DrawerProps {
   className?: string;
   children?: ReactNode;
   isOpen?: boolean;
   onClose?: () => void;
+  lazy?: boolean;
 }
 
 export const Drawer = (props: DrawerProps) => {
-  const { className, children, isOpen, onClose } = props;
+  const { className, children, isOpen, onClose, lazy } = props;
   const { theme } = useTheme();
+
+  const { close, isClosing, isMounted } = useModal({
+    animationDelay: 300,
+    onClose,
+    isOpen,
+  });
 
   const mods: Mods = {
     [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
   };
+
+  if (lazy && !isMounted) {
+    return null;
+  }
 
   return (
     <Portal>
       <div className={classNames(cls.Drawer, mods, [className, theme, ''])}>
-        <Overlay onClick={onClose} />
+        <Overlay onClick={close} />
         <div className={cls.content}>{children}</div>
       </div>
     </Portal>
