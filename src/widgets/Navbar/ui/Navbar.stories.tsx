46cea9736f1e5679ed5769b35711fc54131eb80e
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-06-04 14:51:13 +0700

    fix storybook

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -4,6 +4,10 @@ import { Navbar } from './Navbar';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { http, HttpResponse, delay } from 'msw';
+import { mswDecorator, initialize } from 'msw-storybook-addon';
+
+initialize();
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -26,6 +30,17 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const user = {
+  "id": "1",
+  "first": "Иван",
+  "lastname": "Иванов1",
+  "age": 33,
+  "currency": "USD",
+  "country": "Armenia",
+  "city": "Vologda",
+  "username": "admin",
+  "avatar": "https://lastfm.freetls.fastly.net/i/u/ar0/708e7517998748bac8a19f4a42635124.png"
+};
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Light: Story = {
   args: {},
@@ -44,7 +59,22 @@ Dark.parameters = {
 export const AuthNavbar: Story = {
   args: {},
 };
-AuthNavbar.decorators = [StoreDecorator];
+AuthNavbar.decorators = [StoreDecorator, mswDecorator];
 AuthNavbar.parameters = {
-  state: { user: { authData: {} } },
+  state: { user: { authData: user } },
+  msw: {
+    handlers: [
+      http.get(`${__API__}/notifications`, async () => {
+        await delay(200);
+        return HttpResponse.json([
+          {
+            "id": "1",
+            "title": "Уведомление 1",
+            "description": "Произошло какое-то событие",
+            "userId": "1"
+          }
+        ]);
+      }),
+    ],
+  },
 };
