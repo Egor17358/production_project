be000b28890b28bb448d3876a2d6d95aee17b721
Egor Tebyakin <egor.tebyakin@russianpost.ru>
2025-06-19 17:20:17 +0700

    fix storybook

--- src/entites/Article/ui/ArticleList/ArticleList.stories.tsx
+++ src/entites/Article/ui/ArticleList/ArticleList.stories.tsx
@@ -14,7 +14,7 @@ const meta = {
   component: ArticleList,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'padded',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
